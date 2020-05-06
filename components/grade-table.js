44fd9993efb4c4dfa94339369908fd361cf2dc01
Lawrence Villanueva <boostr29@gmail.com>
2020-05-05 22:57:51 -0700

    fixed table layout in index.html

--- components/grade-table.js
+++ components/grade-table.js
@@ -4,6 +4,14 @@ class GradeTable {
     }
 
     updateGrades(grades) {
+        var tbody = this.tableElement.children("tbody");
+        var $tr = $("<tr>");
+        var $id = $("<td>", {text: grades[0].id});
+        var $id = $("<td>", {text: grades[0].first});
+        var $id = $("<td>", {text: grades[0].id});
+
+        $tr.append($id);
+        tbody.empty().append($tr);
         console.log(grades);
     }
 }
