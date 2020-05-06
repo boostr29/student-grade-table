389f7dcfa1680069a37cd274893f311cebc47474
Lawrence Villanueva <boostr29@gmail.com>
2020-05-05 20:37:38 -0700

    defined getGrades and start method

--- components/app.js
+++ components/app.js
@@ -11,4 +11,22 @@ class App {
     handleGetGradesSucccess(grades){
         console.log(grades)
     }
+
+    getGrades() {
+        var gradesURL = "https://sgt.lfzprototypes.com/api/grades/"
+        var requestPayload = {
+            method: "GET",
+            url: gradesURL,
+            headers: { 
+                "X-Access-Token":"skbKnIuo"
+            }
+        }
+        $.ajax(requestPayload)
+            .done(this.handleGetGradesSucccess)
+            .fail(this.handleGetGradesError);
+    }
+
+    start() {
+        this.getGrades();
+    }
 }
