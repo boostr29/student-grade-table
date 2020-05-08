adf2ee9e8b91486ace7cf4e520dc3960df263b7d
Lawrence Villanueva <boostr29@gmail.com>
2020-05-07 20:39:29 -0700

    modified GradeTable Classadding method to render each row and add delete event listener

--- components/grade-table.js
+++ components/grade-table.js
@@ -5,14 +5,26 @@ class GradeTable {
 
     updateGrades(grades) {
         var tbody = this.tableElement.children("tbody");
-        tbody.empty()
-        grades.forEach(data => {
-            var $row = $("<tr>");
-            var $name = $("<td>", {text: data.name});
-            var $course = $("<td>", {text: data.course});
-            var $grade = $("<td>", {text: data.grade});
-            $row.append($name, $course, $grade);
-            tbody.append($row);
+        tbody.empty();
+        grades.forEach(studentData => {
+            var studentGrades = this.renderGradeRow(studentData, this.deleteGrade);
+            tbody.append(studentGrades);
         });
+    }
+
+    onDeleteClick(deleteGrade) {
+        this.deleteGrade = deleteGrade;
+    }
+
+    renderGradeRow(data, deleteGrade) {
+        var $row = $("<tr>");
+        var $name = $("<td>", {text: data.name});
+        var $course = $("<td>", {text: data.course});
+        var $grade = $("<td>", {text: data.grade});
+        var $delete = $("<td>");
+        var $delBtn = $("<button>", { class:"btn btn-danger", text:"DELETE", click: deleteGrade });
+        $delete.append($delBtn);
+        $row.append($name, $course, $grade, $delete);
+        return $row;
     }
 }
