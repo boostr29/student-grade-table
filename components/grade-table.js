0bab1f64d40851a75ebf9aa74ab9334078c116a5
Lawrence Villanueva <boostr29@gmail.com>
2020-05-09 12:52:33 -0700

    modified to make index.html responsive

--- components/grade-table.js
+++ components/grade-table.js
@@ -23,9 +23,10 @@ class GradeTable {
         var $name = $("<td>", {text: data.name, class:"align-middle"});
         var $course = $("<td>", {text: data.course, class:"align-middle"});
         var $grade = $("<td>", {text: data.grade, class:"align-middle"});
-        var $delete = $("<td>", {class:"align-middle"});
-        var $delBtn = $("<button>", { class:"btn btn-danger", text:"DELETE", click: function() { deleteGrade(data.id) } });
-        $delete.append($delBtn);
+        var $delete = $("<td>", {class:"align-middle text-center"});
+        var $editBtn = $("<i>", { class:"fas fa-edit pr-3", style:"color:green;", click: function() { deleteGrade(data.id) } });
+        var $delBtn = $("<i>", { class:"fas fa-trash-alt", style:"color:red;", click: function() { deleteGrade(data.id) } });
+        $delete.append($editBtn, $delBtn);
         $row.append($name, $course, $grade, $delete);
         return $row;
     }
